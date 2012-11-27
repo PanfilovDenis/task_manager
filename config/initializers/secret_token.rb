284c4aaf9cd8f36e48d8a207d7348f6d0e6049dd
Panfilov Denis <panf.denis@gmail.com>
2012-11-27 22:21:40 +0400

    base RoR application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskManager::Application.config.secret_token = 'b6fff2ce486f89664813aea248126bac087c56333cedef8d27cde05e6a50f5a3b871019b25271c999043db413391c2736e8946ff99c7f542411e597c83469daa'

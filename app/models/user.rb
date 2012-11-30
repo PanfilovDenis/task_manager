b590fadc5e1ee2dd68dd7d36138bddf03774c2e1
Panfilov Denis <panf.denis@gmail.com>
2012-11-30 17:07:29 +0400

    autentification not work

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
   has_many :story
   has_many :story_comment
   attr_accessible :email, :login, :password
+
+  def authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password == password
+      user
+    else
+      nil
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 TaskManager::Application.routes.draw do
   resources :story_comments
-
-
   resources :stories
-
-
   resources :users
+  resources :sessions
+
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
 
 
   # The priority is based upon order of creation:
